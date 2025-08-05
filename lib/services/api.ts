3def2b7be868b3dd81469a223c987869dde58747
HarshDubey1011 <pandetahallu@gmail.com>
2025-08-05 10:43:39 +0530

    added backend url to the env file and updated the url in api.ts file for both the production and development environment

--- lib/services/api.ts
+++ lib/services/api.ts
@@ -12,7 +12,9 @@ export interface RegisterDto {
 }
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || 'http://localhost:8080/api'
+  process.env.NEXT_PUBLIC_API_URL ||
+  process.env.NEXT_PUBLIC_API_URL_LOCAL ||
+  'http://localhost:8080/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
